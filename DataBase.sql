8742a7dbd402d01ef8cc26c713fc1d8b8f3941d8
shynqck <hy.parvizjon@gmail.com>
2024-07-04 14:16:43 +0500

    Initial commit

---
+++ DataBase.sql
@@ -0,0 +1,100 @@
+CREATE DATABASE alif_db;
+
+CREATE TABLE inventory (
+                           id              SERIAL PRIMARY KEY ,
+                           product_name    VARCHAR(55),
+                           category        VARCHAR(55),
+                           quantity        INTEGER CHECK (quantity >= 0) DEFAULT 0,
+                           price           FLOAT,
+                           last_update     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO inventory (product_name, category, quantity, price)
+VALUES
+    ('Говядина', 'Мясо-птица', 500, 86.0),
+    ('Картофель', 'Фрукты и овощи', 1780, 6.20),
+    ('Курица', 'Мясо-птица', 600, 45.0),
+    ('Яблоки', 'Фрукты и овощи', 1500, 15.0),
+    ('Молоко', 'Молочные продукты', 300, 7.5),
+    ('Хлеб', 'Выпечка', 200, 2.5),
+    ('Яйца', 'Молочные продукты', 1000, 1.2),
+    ('Морковь', 'Фрукты и овощи', 1200, 5.0),
+    ('Сыр', 'Молочные продукты', 250, 40.0),
+    ('Рыба', 'Мясо-птица', 400, 55.0),
+    ('Сахар', 'Бакалея', 700, 6.8),
+    ('Рис', 'Бакалея', 1000, 8.5);
+
+SELECT product_name FROM inventory ;
+
+SELECT * FROM inventory
+WHERE category = 'Мясо-птица';
+
+
+CREATE TABLE books(
+                      id SERIAL PRIMARY KEY ,
+                      title VARCHAR(55) NOT NULL,
+                      author VARCHAR(55) NOT NULL,
+                      publication_date DATE,
+                      genre VARCHAR(55),
+                      is_available BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO books (title, author, publication_date, genre, is_available)
+VALUES
+    ('Шахнаме', 'Фирдоуси', '1010-01-01', 'Эпос', TRUE),
+    ('Гулистан', 'Саади', '1258-01-01', 'Поэзия', TRUE),
+    ('Бустан', 'Саади', '1257-01-01', 'Поэзия', TRUE),
+    ('Диуан Хафиза', 'Хафиз', '1390-01-01', 'Поэзия', TRUE),
+    ('Рубаи', 'Омар Хайям', '1120-01-01', 'Поэзия', TRUE),
+    ('Книга царей', 'Ахикар', '500-01-01', 'История', TRUE),
+    ('Трактаты', 'Авиценна', '1020-01-01', 'Философия', TRUE),
+    ('Чахар Мақола', 'Низами-Арузи', '1157-01-01', 'Поэзия', TRUE),
+    ('Зинда-руд', 'Джалал ад-Дин Руми', '1273-01-01', 'Поэзия', TRUE),
+    ('Мемориал', 'Низами Гянджеви', '1200-01-01', 'Поэзия', TRUE),
+    ('Семь планет', 'Низами Гянджеви', '1197-01-01', 'Поэзия', TRUE),
+    ('Сокровища королей', 'Низами Гянджеви', '1202-01-01', 'Поэзия', TRUE),
+    ('Диуан Рудаки', 'Рудаки', '940-01-01', 'Поэзия', TRUE),
+    ('Куллиёт', 'Айни', '1948-01-01', 'Проза', TRUE),
+    ('Ятим вака', 'Айни', '1934-01-01', 'Проза', TRUE);
+
+
+SELECT title FROM books;
+
+SELECT * FROM books
+WHERE author = 'Айни';
+
+SELECT * FROM books
+WHERE genre = 'Поэзия';
+
+SELECT * FROM books
+WHERE is_available = TRUE;
+
+CREATE TABLE students (
+                          id          SERIAL PRIMARY KEY,
+                          first_name  VARCHAR(255),
+                          last_name   VARCHAR(255),
+                          email       VARCHAR(255),
+                          course      INTEGER CHECK (course >= 1 AND course <= 6),
+                          grade       INTEGER CHECK (grade >= 0 AND grade <= 100)
+);
+
+INSERT INTO students (first_name, last_name, email, course, grade)
+VALUES
+    ('Александр', 'Иванов', 'alex.ivanov@example.com', 1, 85),
+    ('Мария', 'Петрова', 'maria.petrova@example.com', 2, 90),
+    ('Дмитрий', 'Смирнов', 'dmitri.smirnov@example.com', 3, 78),
+    ('Ольга', 'Кузнецова', 'olga.kuznetsova@example.com', 4, 92),
+    ('Иван', 'Попов', 'ivan.popov@example.com', 5, 88),
+    ('Анна', 'Соколов', 'anna.sokolov@example.com', 6, 95),
+    ('Петр', 'Лебедев', 'petr.lebedev@example.com', 2, 76),
+    ('Елена', 'Козлов', 'elena.kozlov@example.com', 3, 82),
+    ('Сергей', 'Новиков', 'sergey.novikov@example.com', 1, 89),
+    ('Наталья', 'Морозов', 'natalya.morozov@example.com', 4, 91);
+
+SELECT first_name, last_name FROM students;
+
+SELECT * FROM students
+WHERE course = 2;
+
+SELECT * FROM students
+WHERE grade >= 90;
---
+++ go.mod
@@ -0,0 +1,3 @@
+module DataBase
+
+go 1.22
